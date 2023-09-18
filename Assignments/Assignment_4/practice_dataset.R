b2762350b1ae24957ff36f8ca824e06f877fc08d
olivia-frary <oliviafrary1@gmail.com>
2023-09-18 16:43:32 -0600

    Assignment 4

--- Assignments/Assignment_4/practice_dataset.R
+++ Assignments/Assignment_4/practice_dataset.R
@@ -1,6 +1,9 @@
 library(tidyverse)
 
-data <- read.csv("practice.csv")
+data <- read.csv("Assignments/Assignment_4/fake_dance_data.csv")
+data$PainScale <- as.numeric(data$PainScale)
 
-ggplot(data, aes(x=GS.Fem.pg - GS.Male.Mb, y=lnRRlifespan,)) +
-  geom_point()
+ggplot(data, aes(x=DanceGenre, ..count..)) +
+  geom_bar(aes(fill = InjuryType), position = "dodge") +
+  facet_wrap(~DanceGenre, scales = "free")
+
